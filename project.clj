dfe9b75f1f2b3128ef60ad33f927888785227ea0
Dave Snowdon <dave@davesnowdon.com>
2015-11-10 08:03:59 +0000

    snapshot

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
+  )
--- src/cljex_opencv/core.clj
+++ src/cljex_opencv/core.clj
@@ -1,4 +1,10 @@
-(ns cljex-opencv.core)
+(ns cljex-opencv.core
+    (:import [org.opencv.core Point Rect Mat MatOfRect CvType Size Scalar]
+           org.opencv.imgcodecs.Imgcodecs
+           org.opencv.imgproc.Imgproc
+           org.opencv.objdetect.CascadeClassifier))
+
+
 
 (defn foo
   "I don't do a whole lot."
