a748b0488dbba1f94b321dd833085c9d4f6062ff
Dave Snowdon <dave@davesnowdon.com>
2015-11-15 18:21:48 +0000

    Demo showing use of  classifier to find faces

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject cljex-opencv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Demo using openCV from clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [opencv/opencv "3.0.0"]
+                 [opencv/opencv-native "3.0.0"]]
   :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
   )
--- src/cljex_opencv/core.clj
+++ src/cljex_opencv/core.clj
@@ -4,9 +4,91 @@
            org.opencv.imgproc.Imgproc
            org.opencv.objdetect.CascadeClassifier))
 
+(def FACE-XML "resources/haarcascade_frontalface_default.xml")
 
+(def EYE-XML "resources/haarcascade_eye.xml")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn face-classifier
+  "Creates an instance of an openCV classifier that will detect faces"
+  []
+  (CascadeClassifier. FACE-XML))
+
+(defn eye-classifier
+  "Creates an instance of an openCV classifier that will detect
+  faces. Should be applied to a region of interest which is known to
+  contain a face"
+  []
+  (CascadeClassifier. EYE-XML))
+
+(defn read-image
+  "Create an openCV matrix (Mat) from an image file"
+  [filename]
+  (Imgcodecs/imread filename))
+
+(defn write-image
+  "Write an openCV matric holding image data to a file in one of the
+  supported image formats (PNG & JPEG)"
+  [filename img]
+  (Imgcodecs/imwrite filename img))
+
+(defn colour-to-grayscale
+  "Convert an openCV matrix representing a colour image to a grayscale
+  one of the same size"
+  [img]
+  (let [gray (Mat. (.rows img) (.cols img) CvType/CV_8UC3)]
+    (Imgproc/cvtColor img gray Imgproc/COLOR_BGR2GRAY)
+    gray))
+
+(defn apply-classifier [clr img]
+  (let [result (MatOfRect.)]
+    (.detectMultiScale clr img result)
+    (.toList result)))
+
+(defn roi
+  "Create a matrix representing the region of interest of a larger image
+  defined by an openCV rect"
+  [img rect]
+  (let [min-x (.x rect)
+        max-x (+ min-x (.width rect))
+        min-y (.y rect)
+        max-y (+ min-y (.height rect))]
+    (.submat img min-y max-y min-x max-x)))
+
+(defn min-point
+  "Returns the corner of a rectanges with the smallest x & y values"
+  [rect]
+  (Point. (.x rect) (.y rect)))
+
+(defn max-point
+  "Returns the corner of a rectanges with the largest x & y values"
+  [rect]
+  (Point. (+ (.x rect) (.width rect)) (+ (.y rect) (.height rect))))
+
+(defn apply-classifier-to-roi
+  "Apply a classifier to the region of an image (matrix) defined by an
+  openCv rect"
+  [clr img rect]
+  (let [result (MatOfRect.)
+        roi (roi img rect)]
+    (.detectMultiScale clr roi result)
+    (.toList result)))
+
+(defn faces-and-eyes
+  "Find faces and eyes within those faces in the supplied grayscale image"
+  [img]
+  (let [faces (apply-classifier (face-classifier) img)
+        eyes-in-faces
+        (map #(apply-classifier-to-roi (eye-classifier) img %) faces)]
+    (map (fn [f e] {:face f, :eyes e}) faces eyes-in-faces)))
+
+(defn show-faces-and-eyes
+  "Demonstration function showing how to make use of the values returned
+  by an openCV classified by drawing rectangles around them"
+  [img]
+  (let [gray (colour-to-grayscale img)
+        faces-and-eyes (faces-and-eyes gray)]
+    (doseq [fe faces-and-eyes]
+      (let [{:keys [face eyes]} fe]
+        (Imgproc/rectangle img
+                           (min-point face) (max-point face)
+                           (Scalar. 255.0 0.0 0.0) 2)))))
